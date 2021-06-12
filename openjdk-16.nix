6c551e36e17e39e1e5ea60a2835eaed59ef30c68
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2021-06-13 00:12:39 +0300

    Bump openjdk to 16.0.1+9

--- openjdk-16.nix
+++ openjdk-16.nix
@@ -1,10 +1,24 @@
-{ openjdk16 }:
+{ lib, openjdk16, fetchFromGitHub }:
 
+let
+  version = {
+    feature = "16.0.1";
+    build = "9";
+  };
+in
 openjdk16.overrideAttrs (old: {
+  version = "${version.feature}+${version.build}";
+
+  src = fetchFromGitHub {
+    owner = "openjdk";
+    repo = "jdk${lib.versions.major version.feature}u";
+    rev = "jdk-${version.feature}+${version.build}";
+    sha256 = "1ggddsbsar4dj2fycfqqqagqil7prhb30afvq6933rz7pa9apm2f";
+  };
+
   patches = old.patches ++ [
     ./0001-Set-min_pages-to-1-when-MaxHeapSize-is-equal-to-MinH.patch
     ./0002-Request-alternative-huge-page-size-explicitly-for-SH.patch
     ./0003-Fix-hugetlbfs_sanity_check-not-using-MAP_HUGE_SHIFT.patch
   ];
-  dontStrip = true;
 })
