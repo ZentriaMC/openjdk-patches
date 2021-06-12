3cd9ffb780f6c569a5c7ed94455fa320dd9c96f1
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2021-06-12 23:35:27 +0300

    Fix hugetlbfs_sanity_check not using MAP_HUGE_SHIFT

--- openjdk-16.nix
+++ openjdk-16.nix
@@ -4,6 +4,7 @@ openjdk16.overrideAttrs (old: {
   patches = old.patches ++ [
     ./0001-Set-min_pages-to-1-when-MaxHeapSize-is-equal-to-MinH.patch
     ./0002-Request-alternative-huge-page-size-explicitly-for-SH.patch
+    ./0003-Fix-hugetlbfs_sanity_check-not-using-MAP_HUGE_SHIFT.patch
   ];
   dontStrip = true;
 })
