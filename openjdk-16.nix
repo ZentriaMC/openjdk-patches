dc3319b283f657ee9185bd7fc05ba7b5445af2e9
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2021-12-31 23:33:33 +0200

    Add OpenJDK 17

--- openjdk-16.nix
+++ openjdk-16.nix
@@ -17,8 +17,8 @@ openjdk16.overrideAttrs (old: {
   };
 
   patches = old.patches ++ [
-    ./0001-Set-min_pages-to-1-when-MaxHeapSize-is-equal-to-MinH.patch
-    ./0002-Request-alternative-huge-page-size-explicitly-for-SH.patch
-    ./0003-Fix-hugetlbfs_sanity_check-not-using-MAP_HUGE_SHIFT.patch
+    ./16/0001-Set-min_pages-to-1-when-MaxHeapSize-is-equal-to-MinH.patch
+    ./16/0002-Request-alternative-huge-page-size-explicitly-for-SH.patch
+    ./16/0003-Fix-hugetlbfs_sanity_check-not-using-MAP_HUGE_SHIFT.patch
   ];
 })
---
+++ openjdk-17.nix
@@ -0,0 +1,23 @@
+{ lib, openjdk17, fetchFromGitHub }:
+
+let
+  version = {
+    feature = "17";
+    interim = ".0.1";
+    build = "12";
+  };
+in
+openjdk17.overrideAttrs (old: {
+  version = "${version.feature}${version.interim}+${version.build}";
+
+  src = fetchFromGitHub {
+    owner = "openjdk";
+    repo = "jdk${version.feature}u";
+    rev = "jdk-${version.feature}${version.interim}+${version.build}";
+    sha256 = "1l1jgbz8q7zq66npfg88r0l5xga427vrz35iys09j44b6qllrldd";
+  };
+
+  patches = old.patches ++ [
+    ./17/0002-Request-alternative-huge-page-size-explicitly-for-SH.patch
+  ];
+})
